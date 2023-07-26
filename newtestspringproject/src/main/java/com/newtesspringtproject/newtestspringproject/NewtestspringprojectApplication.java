41ca72243162e7d5b0f9c6354a32c1cf774a5d55
JayPee24 <jp@lapapa88.com>
2023-07-26 13:31:04 +0800

    Initialize Spring test

---
+++ newtestspringproject/src/main/java/com/newtesspringtproject/newtestspringproject/NewtestspringprojectApplication.java
@@ -0,0 +1,13 @@
+package com.newtesspringtproject.newtestspringproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewtestspringprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewtestspringprojectApplication.class, args);
+	}
+
+}
